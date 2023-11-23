c0c92031af26645adb640aad94a752c23452a73c
Diego Quartino <diego.quartino@gmail.com>
2023-11-23 19:53:57 -0300

    Infinite Scroll terminad

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -44,5 +44,10 @@ final appRouter = GoRouter(
       path: '/tutorial',
       builder: (context, state) => const AppTutorialScreen(),
     ),
+    GoRoute(
+      name: InfiniteScrollScreen.name,
+      path: '/infinitescroll',
+      builder: (context, state) => const InfiniteScrollScreen(),
+    ),
   ],
 );
