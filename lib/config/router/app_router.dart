8fd2c0f1a8de4f8956ccaa74aa6ad8234896308a
Diego Quartino <diego.quartino@gmail.com>
2023-11-23 15:46:31 -0300

    tutorial screen added

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -39,6 +39,10 @@ final appRouter = GoRouter(
       path: '/uicontrols',
       builder: (context, state) => const UiControlsScreen(),
     ),
-
+    GoRoute(
+      name: AppTutorialScreen.name,
+      path: '/tutorial',
+      builder: (context, state) => const AppTutorialScreen(),
+    ),
   ],
 );
